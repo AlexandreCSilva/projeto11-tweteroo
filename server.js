da11d05b382b70a0a4419b0ce7462420bd3fb3bb
AlexandreCSilva <alecosta.silva12@gmail.com>
2022-08-27 18:10:10 -0300

    crating the get tweets with pages

--- server.js
+++ server.js
@@ -7,11 +7,29 @@ server.use(cors());
 server.use(express.json());
 
 const user = [];
-const tweet = [];
+const tweets = [];
+const pageTweets = [];
 
 server.post('/sign-up', (req, res) => {
     user.push(req.body);
-    res.send(user);
+    res.send('OK');
+});
+
+server.get('/tweets', (req, res) => {
+    let page = req.query.page;
+    let tweetsOfPage = page * 10;
+
+    if (page < 1){
+        res.status(400).send('Informe uma página válida!');
+    }
+
+    for (let i = 0; i <= 10; i++){
+        if (tweets[tweetsOfPage + i] != null){
+            pageTweets.push(tweets[tweetsOfPage + i]); 
+        }
+    }
+    console.log(pageTweets)
+    res.send(pageTweets);
 });
 
 server.listen(5000, () => console.log('server on'));
