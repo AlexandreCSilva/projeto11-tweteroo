3a1aed5045c638211d945d98015c535c5c03124f
AlexandreCSilva <alecosta.silva12@gmail.com>
2022-08-27 18:20:32 -0300

    crating the post tweets with headers

--- server.js
+++ server.js
@@ -18,18 +18,31 @@ server.post('/sign-up', (req, res) => {
 server.get('/tweets', (req, res) => {
     let page = req.query.page;
     let tweetsOfPage = page * 10;
+    console.log(page)
 
     if (page < 1){
         res.status(400).send('Informe uma página válida!');
     }
 
-    for (let i = 0; i <= 10; i++){
-        if (tweets[tweetsOfPage + i] != null){
-            pageTweets.push(tweets[tweetsOfPage + i]); 
+    for (let i = 10; i >= 0; i--){
+        console.log(tweetsOfPage - i)
+        if (tweets[tweetsOfPage - i] != null){
+            pageTweets.push(tweets[tweetsOfPage - i]); 
         }
     }
+    
     console.log(pageTweets)
     res.send(pageTweets);
+});
+
+server.post('/tweets', (req, res) => {
+    let tweet = {
+        user: req.headers.user,
+        tweet: req.body.tweet
+    }
+    console.log(tweet)
+    tweets.push(tweet);
+    res.send('OK');
 });
 
 server.listen(5000, () => console.log('server on'));
