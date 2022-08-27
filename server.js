363bdbdebf929a7fcc6bd8c70a3f5e8ae6f45b4f
AlexandreCSilva <alecosta.silva12@gmail.com>
2022-08-27 17:39:41 -0300

    making the sign post

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
+
+const user = [];
+const tweet = [];
+
+server.post('/sign-up', (req, res) => {
+    user.push(req.body);
+    res.send(user);
+});
+
+server.listen(5000, () => console.log('server on'));
